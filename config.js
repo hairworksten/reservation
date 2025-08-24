be1217bfab0275b32528980d24717a0d996d779a
hairworksten <keiolinedaibakuha@gmail.com>
2025-08-24 20:00:48 +0900

    Update config.js

--- config.js
+++ config.js
@@ -63,8 +63,8 @@ function getTimeSlotsForDate(dateString) {
             const slotTime = hour * 60 + minute;
             const currentTime = currentHour * 60 + currentMinute;
             
-            // 30分のバッファを設ける（準備時間を考慮）
-            return slotTime > (currentTime + 30);
+            // 15分のバッファを設ける（準備時間を考慮）
+            return slotTime > (currentTime + 15);
         });
     }
     
